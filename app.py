95dbcdf20f8af9daecf490a9741890d7c6ec4b9e
lucasacelino <lucasaacelino@gmail.com>
2025-03-17 23:35:15 -0300

    feat: correção de init em CORS

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ with app.app_context():
 
 app.register_blueprint(loja_bp, url_prefix="/lojas")
 app.register_blueprint(produto_bp, url_prefix="/produtos")
-cors.init_app(app)
+cors(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
