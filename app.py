67f436f3a4b8f7765a9c41e05105d94aecb25985
lucasacelino <lucasaacelino@gmail.com>
2025-03-19 19:54:58 -0300

    refactor: implementando after_request ao CORS

--- app.py
+++ app.py
@@ -18,6 +18,13 @@ with app.app_context():
 
 app.register_blueprint(loja_bp, url_prefix="/lojas")
 app.register_blueprint(produto_bp, url_prefix="/produtos")
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    return response
 # cors(app)
 
 if __name__ == "__main__":
