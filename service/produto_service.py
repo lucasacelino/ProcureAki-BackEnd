400c0e4c64414d35d1dda961a55b55e1236d9f91
lucasacelino <lucasaacelino@gmail.com>
2025-03-23 10:23:46 -0300

    feat: modificando a forma de salvamento de imagens

--- service/produto_service.py
+++ service/produto_service.py
@@ -1,19 +1,26 @@
 from flask import Blueprint, jsonify
 from flask_restful import marshal, marshal_with, reqparse
-from werkzeug.datastructures import FileStorage
-import base64
+from werkzeug.utils import secure_filename
+import os
+# from werkzeug.datastructures import FileStorage
+# import base64
+import requests
 
 from helpers.database import db
 from models.Produto import Produto, produto_fields
 
 produto_bp = Blueprint("produtos", __name__)
 
+UPLOAD_FOLDER = 'uploads'  # Pasta onde as imagens serão salvas
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 parser = reqparse.RequestParser()
 parser.add_argument('nome', type=str, required=True, help="O campo 'nome' é obrigatório.")
 parser.add_argument('preco', type=float, required=True, help="O campo 'preço' deve ser um número válido.")
 parser.add_argument('quantidade', type=int, required=True, help="O campo 'quantidade' deve ser um número inteiro.")
-parser.add_argument('imagem_base64', type=FileStorage, location='files', required=True, help="Imagem do produto é obrigatória")
-# parser.add_argument('imagem_base64', type=str, required=True, help="Imagem do produto é obrigatória")
+parser.add_argument('imagem_base64', type=str, required=True, help="URL da imagem é obrigatória.")
 parser.add_argument('descricao', type=str, required=True, help="O campo 'descricao' é obrigatório.")
 parser.add_argument('loja_id', type=int, required=True, help="O campo 'loja_id' é obrigatório e deve ser um número inteiro.")
 
@@ -24,22 +31,43 @@ def criar_produto():
     try:
         dados = parser.parse_args()
         
-        # arquivo_imagem = dados['imagem_base64']
-        arquivo_imagem: FileStorage = dados['imagem_base64']
-        if arquivo_imagem:
-            imagem_base64 = base64.b64encode(arquivo_imagem.read()).decode('utf-8')
-        else:
-            return {'erro': 'Nenhuma imagem enviada'}, 400
+        # # arquivo_imagem = dados['imagem_base64']
+        # arquivo_imagem: FileStorage = dados['imagem_base64']
+        # if arquivo_imagem:
+        #     imagem_base64 = base64.b64encode(arquivo_imagem.read()).decode('utf-8')
+        # else:
+        #     return {'erro': 'Nenhuma imagem enviada'}, 400
         
         # imagem_base64 = dados['imagem_base64']
         # if not imagem_base64:
         #     return {'erro': 'Nenhuma imagem enviada'}, 400
+        imagem_base64 = dados['imagem_base64']
+        print(imagem_base64)
+        response = requests.get(imagem_base64)
+        if response.status_code != 200:
+            return {'erro': 'Não foi possível baixar a imagem'}, 400
+        
+        filename = secure_filename(imagem_base64.split('/')[-1].split('?')[0])  # Remove query params da URL
+        print(f"Nome do arquivo gerado: {filename}")
+        # Verifica se a extensão é permitida
+        if not allowed_file(filename):
+            return {'erro': 'Formato de imagem não permitido'}, 400
+
+        if not os.path.exists(UPLOAD_FOLDER):
+            os.makedirs(UPLOAD_FOLDER)
+        # Salva a imagem localmente
+        filepath = os.path.join(UPLOAD_FOLDER, filename)
+        with open(filepath, 'wb') as f:
+            f.write(response.content)
+        # print(imagem_base64)
+        
         
         novo_produto = Produto(
             nome = dados['nome'],
             preco = dados['preco'],
             quantidade = dados['quantidade'],
-            imagem_base64 = imagem_base64,
+            # imagem_base64 = imagem_base64,
+            imagem_base64 = f"/{UPLOAD_FOLDER}/{filename}",
             descricao = dados['descricao'],
             loja_id = dados['loja_id']
         )
